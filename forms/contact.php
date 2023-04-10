0efd3166328f28b3e4f9277f416a97dd395db63f
Luban Abdi Susanto <76121572+lubanabdis@users.noreply.github.com>
2023-04-10 19:26:48 +0700

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+ 
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
+    'host' => 'https://lubanabdis.github.io/myresume',
+    'username' => 'lubanabdis',
+    'password' => 'luban1511',
+    'port' => '443'
   );
-  */
+ 
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
